e11613ad4346b74f61ff3a053c6026666b1e4ad7
rabah babaci <4rabah@gmail.com>
2020-03-28 15:22:14 -0700

    install and test use material UI lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className='App'>
       <h3>Test</h3>
+      <Button color='primary'>Test</Button>
     </div>
   );
 }
