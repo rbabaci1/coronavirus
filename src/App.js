4a14364f02f564acf114f579b8383c1b6f91dd32
rabah babaci <4rabah@gmail.com>
2020-03-28 16:41:58 -0700

    create components and utils folde structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className='App'>
-      <h3>Test</h3>
       <Button color='primary'>Test</Button>
     </div>
   );
