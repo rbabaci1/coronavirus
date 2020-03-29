eebb2dc3b3f104e6b7da5ca187d03b1babd100cd
rabah babaci <4rabah@gmail.com>
2020-03-29 16:49:08 -0700

    redner the date for the last updated paragraph

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Moment from 'moment';
+
 import './header.scss';
 
 export default function Header() {
   return (
     <header>
       <h1>COVID-19 CORONAVIRUS PANDEMIC</h1>
-      <p>Last updated: </p>
+      <p>Last updated: {Moment().format('MMMM Do YYYY, h:00 a PT')}</p>
     </header>
   );
 }
