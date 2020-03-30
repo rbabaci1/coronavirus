f3ac02b7c68797387285944956b13b33cd1005bb
rabah babaci <4rabah@gmail.com>
2020-03-29 21:52:39 -0700

    adjust Header, Home and NavBar sizes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Button } from '@material-ui/core';
 
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <NavBar />
       <Route exact path='/' component={Home} />
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import './home.scss';
 export default function Home() {
   return (
     <div className='home'>
-      <Header />
+      <h1>Home</h1>
     </div>
   );
 }
