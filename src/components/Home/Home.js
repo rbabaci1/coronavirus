0ac8501eb902ebcd79b77ab455fd4834aac5ed9b
rabah babaci <4rabah@gmail.com>
2020-03-30 19:28:08 -0700

    in a useEffect fetch for the data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,15 @@ import React, { useState, useEffect } from 'react';
 import './home.scss';
 
 export default function Home() {
+  const [globalCases, setGlobalCases] = useState({});
+
+  useEffect(() => {
+    fetch('https://corona.lmao.ninja/all')
+      .then(response => response.json())
+      .then(data => setGlobalCases(data))
+      .catch(console.error);
+  }, []);
+
   return (
     <div className='home'>
       <h1>Home</h1>
