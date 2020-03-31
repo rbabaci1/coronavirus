ca174958d5bb5714e973064ff8cb14b04235c0a9
rabah babaci <4rabah@gmail.com>
2020-03-30 19:55:51 -0700

    add is Loading state to show when fetching for data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,16 @@ import './home.scss';
 
 export default function Home() {
   const [globalCases, setGlobalCases] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('https://corona.lmao.ninja/all')
       .then(response => response.json())
-      .then(data => setGlobalCases(data))
+      .then(data => {
+        setGlobalCases(data);
+        setIsLoading(false);
+      })
       .catch(console.error);
   }, []);
 
@@ -16,17 +21,23 @@ export default function Home() {
     <div className='home'>
       <section>
         <h2>Coronavirus Cases:</h2>
-        <span className='cases'>{globalCases.cases}</span>
+        <span className='cases'>
+          {isLoading ? 'Loading...' : globalCases.cases}
+        </span>
       </section>
 
       <section>
         <h2>Deaths:</h2>
-        <span className='deaths'>{globalCases.deaths}</span>
+        <span className='deaths'>
+          {isLoading ? 'Loading...' : globalCases.deaths}
+        </span>
       </section>
 
       <section>
         <h2>Recovered:</h2>
-        <span className='recovered'>{globalCases.recovered}</span>
+        <span className='recovered'>
+          {isLoading ? 'Loading...' : globalCases.recovered}
+        </span>
       </section>
     </div>
   );
