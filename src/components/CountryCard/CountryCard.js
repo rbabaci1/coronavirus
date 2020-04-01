4c908db265c1f55388804abb78a6d1113a0ccfe2
rabah babaci <4rabah@gmail.com>
2020-04-01 15:32:06 -0700

    install recharts library

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -10,13 +10,13 @@ export default function CountryCard({ country }) {
         <img src={country.countryInfo.flag} alt='country flag' />
       </p>
 
-      <p>{country.cases}</p>
+      <p className='cases'>{country.cases}</p>
       <p className='new-cases'>+{country.todayCases}</p>
       <p className='recovered'>{country.recovered}</p>
       <p className='critical'>{country.critical}</p>
       <p className='new-deaths'>+{country.todayDeaths}</p>
       <p className='deaths'>{country.deaths}</p>
-      <p>{country.active}</p>
+      <p className='active-cases'>{country.active}</p>
       <p>{country.casesPerOneMillion}</p>
       <p>{country.deathsPerOneMillion}</p>
     </div>
