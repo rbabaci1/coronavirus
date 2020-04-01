de0e6e22410ff45841da97440f377446c38e8db1
rabah babaci <4rabah@gmail.com>
2020-04-01 16:02:09 -0700

    install react video

--- src/components/UsefulInfo/UsefulInfo.js
+++ src/components/UsefulInfo/UsefulInfo.js
@@ -5,7 +5,7 @@ import './usefulInfo.scss';
 export default function UsefulInfo() {
   return (
     <div className='useful-info'>
-      <h1>Info</h1>
+      <h1>MUST WATCH VIDEOS !!!</h1>
     </div>
   );
 }
