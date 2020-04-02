4f0b7d169075db45eddc1d9cab023b868d7fdb19
rabah babaci <4rabah@gmail.com>
2020-04-01 20:35:49 -0700

    add all useful resources links

--- src/components/UsefulInfo/UsefulInfo.js
+++ src/components/UsefulInfo/UsefulInfo.js
@@ -19,7 +19,54 @@ export default function UsefulInfo() {
         <Video url={video} key={i} />
       ))}
 
-      <h1>USEFUL LINKS !!!</h1>
+      <h1>RESOURCES LINKS !!!</h1>
+
+      <ul>
+        <li>
+          <a
+            href='https://www.cdc.gov/coronavirus/2019-ncov/daily-life-coping/checklist-household-ready.html'
+            target='blank'
+          >
+            Checklist To Get Ready
+          </a>
+        </li>
+
+        <li>
+          <a
+            href='https://www.nami.org/getattachment/About-NAMI/NAMI-News/2020/NAMI-Updates-on-the-Coronavirus/COVID-19-Updated-Guide-1.pdf'
+            target='blank'
+          >
+            Nami-Covid-Guide
+          </a>
+        </li>
+
+        <li>
+          <a
+            href='https://childmind.org/coping-during-covid-19-resources-for-parents/'
+            target='blank'
+          >
+            Supporting Families During COVID-19
+          </a>
+        </li>
+
+        <li>
+          <a
+            href='https://www.nasponline.org/resources-and-publications/resources-and-podcasts/school-climate-safety-and-crisis/health-crisis-resources/helping-children-cope-with-changes-resulting-from-covid-19'
+            target='blank'
+          >
+            Helping Children Cope With Changes Resulting from COVID-19
+          </a>
+        </li>
+
+        <li>
+          <a
+            href='https://www.nih.gov/health-information/coronavirus'
+            target='blank'
+          >
+            National Institutes of Health
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
