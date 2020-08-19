427dfd8cf8ccd1da1c4e0c1309dc37d9bc281de4
rabah babaci <4rabah@gmail.com>
2020-08-18 20:31:05 -0700

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useFetch } from '../../hooks/useFetch';
-import { Link } from 'react-router-dom';
-import NumberFormat from 'react-number-format';
-import './home.scss';
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
+import NumberFormat from "react-number-format";
+import "./home.scss";
 
 export default function Home() {
-  const globalCases = useFetch('https://corona.lmao.ninja/v2/all');
+  const globalCases = useFetch("https://corona.lmao.ninja/v2/all");
 
-  if (!globalCases.cases) return <h1 className='status'>Loading...</h1>;
+  if (!globalCases.cases) return <h1 className="status">Loading...</h1>;
 
   return (
-    <div className='home'>
-      <div className='view-countries'>
-        <Link to='/countries'>view by country</Link>
+    <div className="home">
+      <div className="view-countries">
+        <Link to="/countries">view by country</Link>
       </div>
 
       <h1>Global Cases</h1>
 
-      <div className='top'>
+      <div className="top">
         <section>
           <h2>Coronavirus Cases:</h2>
 
           <NumberFormat
             value={globalCases.cases}
-            displayType={'text'}
+            displayType={"text"}
             thousandSeparator={true}
-            renderText={(value) => <span className='cases'>{value}</span>}
+            renderText={value => <span className="cases">{value}</span>}
           />
         </section>
 
@@ -34,22 +34,22 @@ export default function Home() {
 
           <NumberFormat
             value={globalCases.deaths}
-            displayType={'text'}
+            displayType={"text"}
             thousandSeparator={true}
-            renderText={(value) => <span className='deaths'>{value}</span>}
+            renderText={value => <span className="deaths">{value}</span>}
           />
         </section>
       </div>
 
-      <div className='bottom'>
+      <div className="bottom">
         <section>
           <h2>Recovered:</h2>
 
           <NumberFormat
             value={globalCases.recovered}
-            displayType={'text'}
+            displayType={"text"}
             thousandSeparator={true}
-            renderText={(value) => <span className='recovered'>{value}</span>}
+            renderText={value => <span className="recovered">{value}</span>}
           />
         </section>
 
@@ -58,11 +58,9 @@ export default function Home() {
 
           <NumberFormat
             value={globalCases.active}
-            displayType={'text'}
+            displayType={"text"}
             thousandSeparator={true}
-            renderText={(value) => (
-              <span className='active-cases'>{value}</span>
-            )}
+            renderText={value => <span className="active-cases">{value}</span>}
           />
         </section>
       </div>
